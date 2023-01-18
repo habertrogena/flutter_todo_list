53d6ebc4bdff0ab36d440594cf8a68c50d2f36c1
habertrogena <habertorogy12@gmail.com>
2023-01-18 16:06:01 +0300

    feat:created search bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/widget/todo_item.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -6,11 +7,73 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Todo App'),
-      ),
+      backgroundColor: Colors.grey,
+      appBar: _buildAppBar(),
       body: Container(
-        child: Text('this is the home screen'),
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          child: Column(
+            children: [
+              searchBox(),
+              Expanded(
+                child: ListView(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 50, bottom: 20),
+                      child: Text(
+                        'All ToDos',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    TodoItem()
+                  ],
+                ),
+              )
+            ],
+          )),
+    );
+  }
+
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: Colors.black,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
+          border: InputBorder.none,
+          hintText: 'search',
+          hintStyle: TextStyle(color: Colors.grey),
+        ),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: Color.fromARGB(255, 214, 212, 212),
+      title: Row(
+        children: [
+          Icon(
+            Icons.menu,
+            color: Color.fromARGB(193, 246, 13, 13),
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+          )
+        ],
       ),
     );
   }
---
+++ lib/widget/todo_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class TodoItem extends StatelessWidget {
+  const TodoItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('Todo Items'));
+  }
+}
