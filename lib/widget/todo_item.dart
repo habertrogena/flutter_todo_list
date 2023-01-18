c8b04c0353976f63e133e2425b16d6919f6ca262
habertrogena <habertorogy12@gmail.com>
2023-01-18 16:30:08 +0300

    added delete button

--- lib/widget/todo_item.dart
+++ lib/widget/todo_item.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class TodoItem extends StatelessWidget {
@@ -5,6 +6,36 @@ class TodoItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('Todo Items'));
+    return Container(
+      child: ListTile(
+        onTap: () {},
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        tileColor: Colors.white,
+        leading: Icon(Icons.check_box, color: Colors.blue),
+        title: Text(
+          'check mail',
+          style: TextStyle(
+              fontSize: 16,
+              color: Colors.black,
+              decoration: TextDecoration.lineThrough),
+        ),
+        trailing: Container(
+          width: 35,
+          height: 35,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: IconButton(
+            color: Colors.white,
+            iconSize: 18,
+            icon: Icon(Icons.delete),
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
   }
 }
